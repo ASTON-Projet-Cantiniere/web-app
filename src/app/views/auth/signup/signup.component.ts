ad90ba59be3019dec8166769094b1f35e83a0fcc
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2023-01-14 15:20:48 +0100

    Fix issue when select genre in sign-up page

--- src/app/views/auth/signup/signup.component.ts
+++ src/app/views/auth/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RegisterCredentials} from "@shared/models/register-credentials.model";
 import {AuthService} from "@core/services/auth.service";
 
