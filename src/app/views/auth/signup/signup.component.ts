66758f529ad75416e84398668c5a3a1c4fae4d6c
Mucwicki Guillaume <guillaume.mucwicki@gmail.com>
2023-01-14 19:26:07 +0100

    improve style for signin and signup page

--- src/app/views/auth/signup/signup.component.ts
+++ src/app/views/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
-import {RegisterCredentials} from "@shared/models/register-credentials.model";
 import {AuthService} from "@core/services/auth.service";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {RegisterCredentials} from "@shared/models/register-credentials.model";
 
 @Component({
   selector: 'app-signup',
@@ -9,18 +10,57 @@ import {AuthService} from "@core/services/auth.service";
 })
 export class SignupComponent {
 
-  inscription: RegisterCredentials = {
-    name: "",
-    firstname: "",
-    email: "",
-    password: "",
-    sex: 0
+  public loginForm: FormGroup;
+
+  constructor(private authService: AuthService, private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      firstname: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      sex: new FormControl(0, [Validators.required, Validators.pattern('^[0-1]$')]),
+    });
   }
 
-  constructor(private authService: AuthService) {
+  public get name() {
+    return this.loginForm.get('name');
   }
 
-  inscription_methode(){
-    this.authService.signUp(this.inscription);
+  public get firstName() {
+    return this.loginForm.get('firstname');
+  }
+
+  public get email() {
+    return this.loginForm.get('email');
+  }
+
+  public get password() {
+    return this.loginForm.get('password');
+  }
+
+  public get sex() {
+    return this.loginForm.get('sex');
+  }
+
+  private getCredentials(): RegisterCredentials {
+    return {
+      name: this.loginForm.get('name')?.value,
+      firstname: this.loginForm.get('firstName')?.value,
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value,
+      sex: this.loginForm.get('sex')?.value,
+    }
+  }
+
+  public gotoSignin() {
+    this.authService.gotoSignIn();
+  }
+
+  public onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.signUp(this.getCredentials());
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
   }
 }
