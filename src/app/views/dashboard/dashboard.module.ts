95456c76ac12e71a58ea4386fc7917a64a66fa47
Mucwicki Guillaume <guillaume.mucwicki@gmail.com>
2023-01-14 22:52:49 +0100

    Fix bug week-menus-manager routes

--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -7,7 +7,6 @@ import {SharedModule} from "@shared/shared.module";
 import {DashboardComponent} from "@views/dashboard/dashboard.component";
 import {IngredientsManagerComponent} from "@views/dashboard/ingredients-manager/ingredients-manager.component";
 import {MenusManagerComponent} from "@views/dashboard/menus-manager/menus-manager.component";
-import {WeekMenusManagerComponent} from "@views/dashboard/week-menus-manager/week-menus-manager.component";
 import {DashboardCardComponent} from "@views/dashboard/dashboard-card/dashboard-card.component";
 import{ReactiveFormsModule} from "@angular/forms";
 
@@ -24,7 +23,6 @@ import{ReactiveFormsModule} from "@angular/forms";
     DashboardComponent,
     IngredientsManagerComponent,
     MenusManagerComponent,
-    WeekMenusManagerComponent,
     DashboardCardComponent,
   ]
 })
