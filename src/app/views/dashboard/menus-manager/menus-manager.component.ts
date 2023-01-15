494eb4860cdc4d9feaa65397d04ec98291ce42a4
Theo Folquin <theo.folquin.dev@gmail.com>
2023-01-15 23:06:45 +0100

    wip

--- src/app/views/dashboard/menus-manager/menus-manager.component.ts
+++ src/app/views/dashboard/menus-manager/menus-manager.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from '@shared/services/manager/menu.service';
+import { Menu } from '@shared/models/menu.model';
+import { Meal } from '@shared/models/meal.model';
+import { MealService } from '@shared/services/manager/meal.service';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormControl} from "@angular/forms";
 @Component({
   selector: 'app-menus-manager',
   templateUrl: './menus-manager.component.html',
   styleUrls: ['./menus-manager.component.scss']
 })
-export class MenusManagerComponent {
+export class MenusManagerComponent implements OnInit{
+  menus: Menu[]= [];
+  mealList: Meal[] = [];
 
+  //Formulaire
+  formAdd!: FormGroup;
+
+  constructor(private menuService: MenuService, private mealService: MealService, private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.menuService.getAllMenu().subscribe((r: Menu[])=>{
+      this.menus.push(...<[]>r);
+    })
+    this.mealService.getAllMeal().subscribe((r: Meal[]) =>{
+      this.mealList.push(...<[]>r)
+    })
+    console.log(this.menus);
+
+    this.formAdd = this.fb.group({
+      description: [""],
+      availableForWeeksAndDays: this.fb.array([]),
+      label: [""],
+      imagePath: [""],
+      image64: [""],
+      mealIds: this.fb.array([]),
+      priceDF: [0]
+    })
+  }
+  get availableForWeeksAndDays(){
+    return this.formAdd.get("availableForWeeksAndDays") as FormArray;
+  }
+  get mealIds(){
+    return this.formAdd.get("mealIds") as FormArray;
+  }
+  /**
+   * Ajoute une date de disponibilité à la création de menu en cours
+   */
+  addAvailability() {
+    const availableForm = this.fb.group({
+      week: [0],
+      day: [0],
+    });
+    this.availableForWeeksAndDays.push(availableForm);
+    console.log(availableForm);
+  }
+  /**
+   * Ajoute un plat au menu en cours de création
+   */
+  addMeals() {
+    const ids = this.fb.group([]);
+    this.mealIds.push(ids);
+    console.log(ids);
+  }
+
+  /**
+   * Ajoute un menu à la base de données
+   */
+  addMenu(){
+    this.menuService.addMenu({
+      "description": this.formAdd.value.description,
+      "label": this.formAdd.value.label,
+      "image":
+        {
+          "imagePath": this.formAdd.value.imagePath,
+          "image64": this.formAdd.value.image64
+        },
+      "priceDF": this.formAdd.value.priceDF,
+      "availableForWeeksAndDays": {
+        "values": [...this.formAdd.value.availableForWeeksAndDays]
+      },
+      "mealIds": [
+        ...this.formAdd.value.mealIds
+      ]
+    }).subscribe();
+  }
+
+  /**
+   * Supprime le menu sélectionné
+   * @param menuId number
+   */
+  public removeMenu(menuId: number){
+    this.menuService.deleteMenu(menuId).subscribe();
+  }
 }
--- src/app/views/dashboard/menus-manager/menus-manager.module.ts
+++ src/app/views/dashboard/menus-manager/menus-manager.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddMenuComponent } from './add-menu/add-menu.component';
 import { MenusManagerComponent } from './menus-manager.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 
@@ -11,7 +12,9 @@ import { MenusManagerComponent } from './menus-manager.component';
     MenusManagerComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class MenusManagerModule { }
