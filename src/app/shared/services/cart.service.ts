b491c47c540b8e06057101f01d34a5901135381e
Mucwicki Guillaume <guillaume.mucwicki@gmail.com>
2023-01-15 21:46:59 +0100

    add cart page and service

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -3,44 +3,56 @@ import {Menu} from "@shared/models/menu.model";
 import {BroadcasterService} from "@core/services/broadcaster.service";
 import {map, Observable, Subject, takeUntil} from "rxjs";
 import {CONSTANTS} from "@core/constants";
+import {WeekDay, CartItem} from "@shared/models/cart-item.model";
 
 @Injectable({providedIn: 'root'})
 export class CartService implements OnDestroy {
   private $destroy = new Subject();
-  private $cartState = this.broadcaster.on<Menu[]>(CONSTANTS.CART_STATE).pipe(takeUntil(this.$destroy));
-  private cart: Menu[] | undefined;
+  private $cartState = this.broadcaster.on<CartItem[]>(CONSTANTS.CART_STATE).pipe(takeUntil(this.$destroy));
+  private cart: CartItem[] = [];
   private cartCount: number = 0;
 
   constructor(private broadcaster: BroadcasterService) {
-    this.$cartState.subscribe((cart: Menu[]) => {
+    this.$cartState.subscribe((cart: CartItem[]) => {
       this.cart = cart;
       this.cartCount = cart.length;
     });
-    this.initCartState();
+    this.emitCartCount();
   }
 
   ngOnDestroy(): void {
     this.$destroy.complete();
   }
 
-  listenCartState(): Observable<Menu[]> {
+  /**
+   * Listener of the cart state.
+   * @return {Observable<CartItem[]>}
+   */
+  listenCartState(): Observable<CartItem[]> {
     return this.$cartState;
   }
 
+  /**
+   * Listener of the cart count.
+   * @return {Observable<number>}
+   */
   listenCartCount(): Observable<number> {
     return this.$cartState.pipe(map(() => this.cartCount));
   }
 
-  private initCartState() {
+  /**
+   * Emit the cart count.
+   */
+  emitCartCount() {
     this.broadcaster.broadcast(CONSTANTS.CART_STATE, this.readCartInLocalStorage());
   }
 
   /**
    * Read the cart in the local storage.
-   * @return {Menu[]}
+   * @return {CartItem[]}
    * @private
    */
-  private readCartInLocalStorage(): Menu[] {
+  private readCartInLocalStorage(): CartItem[] {
     return JSON.parse(localStorage.getItem('cart_items') || '[]');
   }
 
@@ -49,39 +61,38 @@ export class CartService implements OnDestroy {
    * @param cart
    * @private
    */
-  private writeCartInLocalStorage(cart: Menu[]): void {
+  private writeCartInLocalStorage(cart: CartItem[]): void {
     localStorage.setItem('cart_items', JSON.stringify(cart));
+    this.emitCartCount();
   }
 
   /**
    * Add a menu to the cart.
-   * @param menu
+   * @param cartItem
    */
-  addMenuToCart(menu: Menu) {
+  addItemToCart(cartItem: CartItem) {
     const cart = this.readCartInLocalStorage();
-    cart.push(menu);
+    cart.push(cartItem);
     this.writeCartInLocalStorage(cart);
-    this.initCartState();
   }
 
   /**
    * Get the cart.
-   * @return {Menu[]}
+   * @return {CartItem[]}
    */
-  getCart(): Menu[] {
+  getCart(): CartItem[] {
     return this.readCartInLocalStorage();
   }
 
   /**
    * Remove a menu from the cart.
-   * @param menu
+   * @param item
    */
-  removeMenuFromCart(menu: Menu) {
+  removeItemFromCart(item: CartItem) {
     const cart = this.readCartInLocalStorage();
-    const index = cart.findIndex((cartMenu) => cartMenu.id === menu.id);
+    const index = cart.findIndex((cartItem) => cartItem.menu.id === item.menu.id);
     cart.splice(index, 1);
     this.writeCartInLocalStorage(cart);
-    this.initCartState();
   }
 
   /**
@@ -89,6 +100,32 @@ export class CartService implements OnDestroy {
    * @return {number}
    */
   getTotalPrice(): number {
-    return this.readCartInLocalStorage().reduce((total, menu) => total + menu.priceDF, 0);
+    return this.cart.reduce((total, cartItem) => total + cartItem.menu.priceDF * cartItem.quantity, 0);
+  }
+
+  /**
+   * Get the total quantity of a menu in the cart.
+   * @param menu
+   * @return {number}
+   */
+  getTotalQuantity(item: CartItem): number {
+    return this.readCartInLocalStorage().filter((cartItem) => cartItem.menu.id === item.menu.id).length;
+  }
+
+  /**
+   * Get the menus by day.
+   * @param day
+   * @return {Menu[]}
+   */
+  getItemsByDay(day: WeekDay): CartItem[] {
+    return this.cart.filter((cartItem) => cartItem.day === day);
+  }
+
+  /**
+   * Clear the cart.
+   * @return {void}
+   */
+  clearCart(): void {
+    this.writeCartInLocalStorage([]);
   }
 }
